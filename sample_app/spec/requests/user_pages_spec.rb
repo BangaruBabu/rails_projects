1d15891fb2f1340482f5da531f61d9f1bf539430
BangaruBabu <yoursbangarubabu@gmail.com>
2015-02-10 13:19:57 +0530

    Finished Layout and Routes

---
+++ sample_app/spec/requests/user_pages_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+describe "User pages" do
+  subject { page }
+  describe "signup page" do
+    before { visit signup_path }
+    it { should have selector( 'h1' , text: 'Sign up' ) }
+    it { should have selector( 'title' , text: full_title( 'Sign up' )) }
+  end
+end
